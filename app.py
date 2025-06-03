f92ff9ea5ca198605f4a4e59d8193d746c04e7ed
nadirm5 <nadir.med55555@gmail.com>
2025-06-03 09:46:05 +0000

    Update app.py

--- app.py
+++ app.py
@@ -36,4 +36,5 @@ def ask():
         return jsonify({"error": "Mode invalide"}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
